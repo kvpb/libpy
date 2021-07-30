ca1e249d87a286e27fe1d1dc2ec6ada537205b2e
Karl <local-part@domain>
2021-07-30 23:51:28 +0200

    Factorial.py created.

---
+++ Factorial.py
@@ -0,0 +1,21 @@
+def nbang(n):
+  if n == 0:
+    return 1
+  return n * nbang( n - 1 ) # factorial definition: n! = n * ( n - 1 )!
+# KVPB's RCSVFACT
+
+#print(nbang(  0 ))
+#print(nbang(  1 ))
+#print(nbang(  2 ))
+#print(nbang(  3 ))
+#print(nbang( 20 ))
+#   debug code
+
+# Factorial.py
+# Factorial number
+#
+# Karl V. P. Bertin `kvpb`
+# +33 A BB BB BB BB
+# local-part@domain
+# https://www.linkedin.com/in/karlbertin
+
