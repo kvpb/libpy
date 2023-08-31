a7b65040588b88dd573e993772859335d47cd73f
Karl <local-part@domain>
2023-08-31 23:11:11 +0200

    Factorial.py renamed factorial.py according to the Python Software Foundation and MathWorks. https://peps.python.org/pep-0008/#package-and-module-names https://www.mathworks.com/help/matlab/ref/factorial.html

---
+++ factorial.py
@@ -0,0 +1,21 @@
+def factorial(n):
+  if n == 0:
+    return 1
+  return n * factorial( n - 1 ) # factorial definition: n! = n * ( n - 1 )!
+# KVPB's RCSVFACT
+
+#print(factorial(  0 ))
+#print(factorial(  1 ))
+#print(factorial(  2 ))
+#print(factorial(  3 ))
+#print(factorial( 20 ))
+#   debug code
+
+# factorial.py
+# factorial of an integer
+#
+# Karl V. P. B. `kvpb`
+# +33 A BB BB BB BB
+# local-part@domain
+# https://www.linkedin.com/in/
+
